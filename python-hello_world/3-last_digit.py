4d3ed9e39f2da033088c8382ad942e2ffd7b15da
yassine-fawzi <fawziyassine0@gmail.com>
2023-11-30 22:25:37 +0100

    added readme file

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -10,4 +10,4 @@ else:
     if last_digit > 5:
         print("Last digit of {:d} is {:d} and is greater than 5".format(number, last_digit))
     else:
-        print("Last digit of {:d} is {:d} and less than 6 and not 0".format(number, last_digit))
+        print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(number, last_digit))
