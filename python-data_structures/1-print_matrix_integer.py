5338fd9cdce267ec0af806c025efd0e48be45d7b
yassine-fawzi <fawziyassine0@gmail.com>
2023-12-07 20:29:43 +0100

    push

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -6,11 +6,3 @@ def print_matrix_integer(matrix=[[]]):
             else:
                 print(" {:d} ".format(col), end="")
         print()
-
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-print_matrix_integer(matrix)
