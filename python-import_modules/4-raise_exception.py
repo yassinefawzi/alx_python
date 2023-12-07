c8464e7329adfb642245c79c78e0a0f50f2d5297
yassine-fawzi <fawziyassine0@gmail.com>
2023-12-07 19:57:28 +0100

    push

--- python-import_modules/4-raise_exception.py
+++ python-import_modules/4-raise_exception.py
@@ -1,6 +1,7 @@
 def raise_exception():
     raise TypeError("Exception raised")
-try:
-    raise_exception()
-except TypeError as te:
-    print("Exception raised")
+if __name__ == "__main__":
+    try:
+        raise_exception()
+    except TypeError as te:
+        print("Exception raised")
--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -1,6 +1,7 @@
 def raise_exception_msg(message=""):
     raise NameError(message)
-try:
-    raise_exception_msg("C is fun")
-except NameError as ne:
-    print(ne)
+if __name__ == "__main__":
+    try:
+        raise_exception_msg("C is fun")
+    except NameError as ne:
+        print(ne)
