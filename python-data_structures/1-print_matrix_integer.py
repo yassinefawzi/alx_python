c27a64c617b3c46f440737fc764b5a179dd8988f
yassine-fawzi <fawziyassine0@gmail.com>
2023-12-07 22:39:11 +0100

    push

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,8 +1,8 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for col in row:
-            if col < 0:
-                print("{:d} ".format(col), end="")
+            if len(row) == len(row) - 1:
+                print("{:d}".format(col), end="")
             else:
-                print(" {:d} ".format(col), end="")
+                print("{:d} ".format(col), end="")
         print()
