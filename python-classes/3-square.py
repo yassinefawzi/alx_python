afbff146e507ef372968d7c98b951962cb51561e
yassine-fawzi <fawziyassine0@gmail.com>
2024-01-08 03:21:20 +0100

    finished

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -10,16 +10,16 @@ class Square:
         self.__size = size
         
         "getter and setter methods"
-        @property
-        def size(self):
-            return self.__size
-        @size.setter
-        def size(self, value):
-            if not isinstance(value,int):
-                raise TypeError("size must be an integer")
-            if value < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = value
+    @property
+    def size(self):
+        return self.__size
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
     
     "area method that return the square root"        
     def area(self):
