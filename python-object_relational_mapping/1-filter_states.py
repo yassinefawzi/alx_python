0931e5191bbb0f727a38c2bd05d18ba7f27313a2
yassine-fawzi <fawziyassine0@gmail.com>
2024-01-18 02:26:04 +0100

    push

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,23 +1,23 @@
 import MySQLdb as host
+from sys import argv
 
 # defining our function states
-def states(userr, passwrd, data):
-    database = None;
-    try:
-        # making connection to the localhost
-        database = host.connect(host="localhost", port=3306, user=userr, password=passwrd, database=data)
-        cursor = database.cursor()
-        query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC"
-        cursor.execute(query)
-        result = cursor.fetchall()
-        
-        # printing the results
-        for i in result:
-            print(i)
-    except host.Error as err:
-        # printing an error message using erno to explain the type of error
-        print(f"Error couldn't connect to localhost: {err}")
-    finally :
-        # ending the database connection
-        if database:
-            database.close()
+database = None;
+try:
+    # making connection to the localhost
+    database = host.connect(host="localhost", port=3306, user=argv[1], password=argv[2], database=argv[3])
+    cursor = database.cursor()
+    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC"
+    cursor.execute(query)
+    result = cursor.fetchall()
+
+    # printing the results
+    for i in result:
+       print(i)
+except host.Error as err:
+    # printing an error message using erno to explain the type of error
+    print(f"Error couldn't connect to localhost: {err}")
+finally :
+    # ending the database connection
+    if database:
+        database.close()
--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,23 +1,23 @@
 import MySQLdb as host
+from sys import argv
 
 # defining our function states
-def states(userr, passwrd, data, name_searched):
-    databe = None
-    try:
-        # making connection to the localhost
-        databe = host.connect(host="localhost", port=3306, user=userr, password=passwrd, database=data)
-        cursor = databe.cursor()
-        query = "SELECT * FROM states WHERE name = %s ORDER BY states.id ASC"
-        cursor.execute(query, (name_searched,))
-        result = cursor.fetchall()
-        
-        # printing the results
-        for i in result:
-            print(i)
-    except host.Error as err:
-        # printing an error message using erno to explain the type of error
-        print(f"couldn't connect to localhost: {err}")
-    finally:
-        # ending the database connection
-        if databe:
-            databe.close()
+databe = None
+try:
+    # making connection to the localhost
+    databe = host.connect(host="localhost", port=3306, user=argv[1], password=argv[2], database=argv[3])
+    cursor = databe.cursor()
+    query = "SELECT * FROM states WHERE name = %s ORDER BY states.id ASC"
+    cursor.execute(query, (name_searched,))
+    result = cursor.fetchall()
+ 
+    # printing the results
+    for i in result:
+        print(i)
+except host.Error as err:
+    # printing an error message using erno to explain the type of error
+    print(f"couldn't connect to localhost: {err}")
+finally:
+    # ending the database connection
+    if databe:
+        databe.close()
