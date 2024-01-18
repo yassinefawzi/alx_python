4d83ab4633621ea786274fb4fe7d0f6295404b3e
yassine-fawzi <fawziyassine0@gmail.com>
2024-01-18 22:27:04 +0100

    push

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -16,7 +16,7 @@ try:
         
     # printing the results
     for i in result:
-        print(', '.join(i[0]))
+        print(', '.join(i))
 except host.Error as err:
     # printing an error message using erno to explain the type   of error
     print(f"couldn't connect to localhost: {err}")
