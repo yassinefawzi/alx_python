75972d83b10b1b8e7c39bc05eb3ca0761959f83a
yassine-fawzi <fawziyassine0@gmail.com>
2024-01-18 02:39:32 +0100

    push

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -7,7 +7,7 @@ try:
     # making connection to the localhost
     databe = host.connect(host="localhost", port=3306, user=argv[1], password=argv[2], database=argv[3])
     cursor = databe.cursor()
-    query = query = '''SELECT cities.id, cities.name, states.name, FROM cities
+    query = query = '''SELECT cities.id, cities.name, states.name FROM cities
                 JOIN states ON cities.state_id = states.id
                 ORDER BY cities.state_id ASC, cities.id ASC;
             '''
