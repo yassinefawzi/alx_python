fcf4d5dd6929c8a642b10cb3a852d138ffb17319
yassine-fawzi <fawziyassine0@gmail.com>
2024-01-24 23:42:39 +0100

    push

---
+++ python-web_framework/1-hbnb_route.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+#assigning the app route to the root with ignoring slashes
+@app.route('/', strict_slashes = False)
+def hello_hbnb():
+    # creating a function that return text if called by root
+    return ("Hello HBNB!")
+
+@app.route('/hbnb', strict_slashes = False)
+def hbnb():
+    # creating a function that return text if called by hbnb
+    return("HBNB")
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
---
+++ python-web_framework/3-python_route.py
@@ -0,0 +1,27 @@
+from flask import Flask
+
+app = Flask(__name__)
+#assigning the app route to the root with ignoring slashes
+@app.route('/', strict_slashes = False)
+def hello_hbnb():
+    # creating a function that return text if called by root
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes = False)
+def hbnb():
+    # creating a function that return text if called by hbnb
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes = False)
+def c_text(text):
+    # creating a function that return text with the string next to it
+    text = text.replace('_', ' ')
+    return f"C {text}"
+
+def python_text(text='is_cool'):
+    # creating a function that return text
+    text = text.replace('_', ' ')
+    return f'Python {text}'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
