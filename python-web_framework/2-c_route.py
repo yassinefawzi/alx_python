94d2f5333f054c906cc3f2205f3e3f7fdf8b5fb7
yassine-fawzi <fawziyassine0@gmail.com>
2024-01-25 00:20:41 +0100

    work

--- python-web_framework/2-c_route.py
+++ python-web_framework/2-c_route.py
@@ -1,13 +1,19 @@
+'''
+importing flask and starting the app
+'''
+
 from flask import Flask
 
-'''assigning flask to app'''
 app = Flask(__name__)
 
-'''assigning the app route to the root with ignoring slashes'''
 @app.route('/', strict_slashes = False)
 def hello_hbnb():
-    '''creating a function that return text if opened by root'''
-    return ("Hello HBNB!")
+    """
+    this function return a string with Hello HBNB! if it's accessed
+    with the root
+    """
+    return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes = False)
 def hbnb():
--- python-web_framework/5-number_template.py
+++ python-web_framework/5-number_template.py
@@ -1,12 +1,17 @@
+'''
+importing flask and starting the app
+'''
+
 from flask import Flask, render_template
 
-'''assigning flask to app'''
 app = Flask(__name__)
 
-'''assigning the app route to the root with ignoring slashes'''
 @app.route('/', strict_slashes = False)
 def hello_hbnb():
-    '''creating a function that return text if called by root'''
+    """
+    this function return a string with Hello HBNB! if it's accessed
+    with the root
+    """
     return "Hello HBNB!"
 
 @app.route('/hbnb', strict_slashes = False)
